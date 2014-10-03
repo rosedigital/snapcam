5b4ed226edd76b71ea45e148d581bbac0e9cb8cb
Zac Colley <hi@zaccolley.com>
2014-10-03 22:54:35 +0100

    Improvments to effects

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var gm = require('gm'),
 	takePicture = require('./takePicture.js'),
 	downloadSnaps = require('./downloadSnaps.js'),
 
+	moment = require('moment'),
+
 	dir = '.',
 	inPath = dir + '/cam.jpeg',
     outPath = dir + '/out.jpeg';
@@ -20,9 +22,13 @@ setInterval(function(){
 	});
 }, 3000);
 
+// test image shit
+// fixImage(inPath, { width: 640, height: 480 }, function(){});
+
 function takeAndSend(usernames){
 	takePicture(inPath, function(){
 		gm(inPath).size(function(err, img){
+			
 			fixImage(inPath, img, function(){
 				sendSnap(10, outPath, usernames);
 			});
@@ -38,7 +44,16 @@ function fixImage(inPath, img, callback){
 	gm(inPath)
 		.rotate('black', 90)
 		.crop(x - 120, y, 0, 0)
-	
+  		.colorize(Math.random()*100, Math.random()*100, Math.random()*100)
+  		.contrast(2)
+
+		.fontSize(20)
+		.font('Courier')
+		.fill('#f5f5f5')
+		.drawText(15, 30, 'innospacecam1', 'NorthWest')
+		.drawText(15, 60, moment().format('h:mm A'), 'NorthWest')
+		.drawText(15, 85, moment().format('D/M/YY'), 'NorthWest')
+		.drawText(0, 80, 'cam.rose.digital', 'South')
 
 		.write(outPath, function(err){
 			if (err) return console.dir(arguments)
@@ -47,6 +62,7 @@ function fixImage(inPath, img, callback){
 				gm().subCommand('composite')
 					.gravity('center')
 					.in('-compose', 'Over', './watermark.png', outPath)
+
 
 					.write(outPath, function(err){
 						if (err) return console.dir(arguments)
